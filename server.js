9f3df0cd751745cd6d81ef796a8ca375468a1962
YoneS2bwa <luisotavioss20001@gmail.com>
2025-07-21 15:11:54 -0300

    UPDATE

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-// Importa a instância do Sequelize configurada e o modelo de usuário
 const sequelize = require('./config/db');
 require('dotenv').config();
-const express = require('express')
- 
-const userRoutes = require('./routes/userRoutes')
-const contactRoutes = require('./routes/contactRoutes')
-const authRoutes = require('./routes/authRoutes')
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
- 
-app.use(express.json());
- 
-app.get('/', (req, res)=> res.send('api funcionando'))
- 
-app.use('/api/users', userRoutes)
-app.use('/api/contact', contactRoutes)
-app.use('/api/auth', authRoutes)
 
- 
-const PORT = process.env.PORT;
- 
+app.use(cors());
+app.use(express.json());
+
+const userRoutes = require('./routes/userRoutes');
+const contactRoutes = require('./routes/contactRoutes');
+const authRoutes = require('./routes/authRoutes');
+
+app.get('/', (req, res) => res.send('api funcionando'));
+
+app.use('/api/users', userRoutes);
+app.use('/api/contact', contactRoutes);
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
 sequelize.authenticate()
   .then(() => {
-    console.log('servidor online e conectado com o DB')
+    console.log('servidor online e conectado com o DB');
     return sequelize.sync();
   })
-  .then(() =>{
-    console.log('banco de dados sincronizado')
-    app.listen(PORT, () => console.log("SERVIDOR RODANDO NA PORTA: " + PORT))
-  }).catch(erro => console.log("Erro interno do servidor", erro))
+  .then(() => {
+    console.log('banco de dados sincronizado');
+    app.listen(PORT, () => console.log("SERVIDOR RODANDO NA PORTA: " + PORT));
+  })
+  .catch(erro => console.log("Erro interno do servidor", erro));
